d67c12e5546b64132b9acd4b8a14e275394655dd
DevilExileSu <39342795+DevilExileSu@users.noreply.github.com>
2021-03-14 14:44:01 +0800

    first commit

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,7 +1,5 @@
 import torch
 from collections import Counter
-from sklearn.metrics import classification_report
-from sklearn import metrics
 
 EPSILON = 1e-10
 
@@ -59,21 +57,3 @@ def mse(y_pred, y_true):
 def mae(y_pred, y_true):
     assert len(y_pred) == len(y_true)
     return torch.mean(torch.abs(y_true - y_pred))
-
-def report(y_pred, y_true, logger):
-    # logger.debug(classification_report(y_true, y_pred))
-    logger.info(metrics.f1_score(y_true, y_pred, average="micro"))
-    logger.info(metrics.f1_score(y_true, y_pred, average="macro"))
-
-
-if __name__=="__main__":
-    y_true = torch.tensor([1,1,2,3,4,1,2,3])
-    y_pred = torch.tensor([1,1,1,2,4,2,2,3])
-    pre = precision(y_pred, y_true)
-    acc = accuracy(y_pred, y_true)
-    rc = recall(y_pred, y_true)
-    f1 = f1Score(y_pred, y_true)
-    print(acc)
-    for i in f1:
-        print(pre[i], rc[i], f1[i])
-    print(classification_report(y_true, y_pred))
